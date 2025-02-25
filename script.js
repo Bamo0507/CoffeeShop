077e5abdda57a9fc0fbe9a5cdb4bc504e3755b49
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2025-02-25 14:26:07 -0600

    Testimonials section was added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Menu options
 const menuOpenButton = document.querySelector("#menu-open-button");
 
 const menuCloseButton = document.querySelector("#menu-close-button");
@@ -10,3 +11,39 @@ menuOpenButton.addEventListener("click", () => {
 menuCloseButton.addEventListener("click", () => 
     menuOpenButton.click() /*Trigerea que se acitve el open button click listener, entnoces encoje el menu*/
 );
+
+// Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    
+    spaceBetween: 10,
+
+    grabCursor: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    //   Activates the buttons at the bottom
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive Breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+  });
