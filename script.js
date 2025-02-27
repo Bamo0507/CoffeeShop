3e2077f4ecc028877e89864953b156f6a680c0e0
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2025-02-26 18:28:49 -0600

    navigation was included

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 // Menu options
+
+// Selector en JS para una sola cosa
 const menuOpenButton = document.querySelector("#menu-open-button");
 
 const menuCloseButton = document.querySelector("#menu-close-button");
 
+// Selector en JS cuando se quiere seleccionar todo lo de una cosa
+const navLinks = document.querySelectorAll(".nav-menu .nav-link")
+
+// Toggle mobile menu visibility with close button
 menuOpenButton.addEventListener("click", () => {
-    // Toggle mobile menu visibility
     document.body.classList.toggle("show-mobile-menu")
 });
 
+// Toggle mobile menu visibility with open button
 menuCloseButton.addEventListener("click", () => 
     menuOpenButton.click() /*Trigerea que se acitve el open button click listener, entnoces encoje el menu*/
 );
+
+// Toggle mobile menu when clicking any of the nav link options
+navLinks.forEach(link => {
+  link.addEventListener("click", () => 
+    menuOpenButton.click());
+})
 
 // Swiper
 const swiper = new Swiper('.slider-wrapper', {
